5ec9c34ca630fb0480c7c5586178933e209d7729
jrvz9714 <jrvz9714@hotmail.com>
2019-06-19 14:54:34 -0600

    Primer Avance - BL.

---
+++ BL/Solicitudes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Solicitudes
+    {
+        public int CodigoSolicitud { get; set; }
+        public string NombreSolicitante { get; set; }
+        public int SevicioSolicitado { get; set; }
+        public string Telefono { get; set; }
+        public int Fecha { get; set; }
+        public string Cedula { get; set; }
+        public string Correo { get; set; }
+    }
+}
