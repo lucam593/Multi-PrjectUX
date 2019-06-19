72807ea17e6c1cafd2442bd9228f13ac1fc62557
jrvz9714 <jrvz9714@hotmail.com>
2019-06-19 14:57:49 -0600

    Avance BL

---
+++ BL/Categorias.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Categorias
+    {
+        public int CodigoCategoria { get; set; }
+        public string NombreCategoria { get; set; }
+        public string DescripcionCategoria { get; set; }
+
+        public Categorias(int codigoCategoria, string nombreCategoria, string descripcionCategoria)
+        {
+            CodigoCategoria = codigoCategoria;
+            NombreCategoria = nombreCategoria;
+            DescripcionCategoria = descripcionCategoria;
+        } 
+
+        public Categorias()
+        {
+
+        }
+
+    }
+}
---
+++ BL/Servicios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Servicios
+    {
+        public int CodigoServicio { get; set; }
+        public string NombreServicio { get; set; }
+        public string TipoServicio { get; set; }
+        public string DescripcionServicio { get; set; }
+        public int Precio { get; set; }
+
+        public Servicios(int codigoServicio, string nombreServicio, string tipoServicio, 
+            string descripcionServicio, int precio)
+        {
+            CodigoServicio = codigoServicio;
+            NombreServicio = nombreServicio;
+            TipoServicio = tipoServicio;
+            DescripcionServicio = descripcionServicio;
+            Precio = precio;
+        }
+
+        public Servicios()
+        {
+
+        }
+    }
+}
